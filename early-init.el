b5a17e86dd887063da5cf1e3bafaeb56f9776498
lanceberge <bergeron.lance6@gmail.com>
2020-08-07 15:01:11 -0400

    Using straight as package manager > package.el

--- early-init.el
+++ early-init.el
@@ -16,24 +16,42 @@
 
 (setq frame-inhibit-implied-resize t)
 
-(eval-when-compile
-  (require 'package))
+;; (eval-when-compile
+;;   (require 'package))
 
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")
-			 ("melpa" . "http://melpa.org/packages/")
-			 ("melpa-stable" . "http://stable.melpa.org/packages/"))
-      load-prefer-newer t)
-(package-initialize)
+;; (setq package-archives '(("org" . "http://orgmode.org/elpa/")
+;; 			 ("melpa" . "http://melpa.org/packages/")
+;; 			 ("melpa-stable" . "http://stable.melpa.org/packages/"))
+;;       load-prefer-newer t)
+;; (package-initialize)
 
 ;; make sure use-package is installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 
-(setq-default use-package-always-ensure t)
+;; (setq-default use-package-always-ensure t)
 
-(eval-when-compile
-  (require 'use-package))
+;; (eval-when-compile
+;(defvar bootstrap-version)
+;   (require 'use-package))
+
+;; Bootstrap straight.el
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
+
+(straight-use-package 'use-package)
 
 (use-package gruvbox-theme
   :config
