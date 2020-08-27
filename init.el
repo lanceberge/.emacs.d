380a448a6b4e1fc2db4968e8966ecd47f49f6c22
lanceberge <bergeron.lance6@gmail.com>
2020-08-27 11:57:10 -0400

    Miscellaneous changes

--- init.el
+++ init.el
@@ -2,6 +2,12 @@
 (defconst config-org (expand-file-name "README.org" user-emacs-directory))
 (defconst config-el  (expand-file-name "config.el" user-emacs-directory))
 
+(defconst IS-LINUX (eq system-type 'gnu/linux))
+(defconst IS-WINDOWS (memq system-type '(cygwin windows-nt ms-dos)))
+
+(when (and IS-WINDOWS (null (getenv "HOME")))
+  (setenv "HOME" (getenv "USERPROFILE")))
+
 (unless (file-exists-p config-el)
   (require 'org)
   (org-babel-tangle-file config-org config-el))
@@ -9,5 +15,5 @@
 (load-file config-el)
 
 (when (require 'time-date nil t)
-    (message "Emacs startup time: %.2f seconds."
-               (time-to-seconds (time-since emacs-load-start-time))))
+  (message "Emacs startup time: %.2f seconds."
+           (time-to-seconds (time-since emacs-load-start-time))))
