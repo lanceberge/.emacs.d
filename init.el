8c28817778259fba8a74a3019c3cd9862f0b3cbd
lanceberge <bergeron.lance6@gmail.com>
2020-08-29 23:11:20 -0400

    Refactoring, startup time after-init, regex searches for swiper

--- init.el
+++ init.el
@@ -2,15 +2,14 @@
 (defconst config-org (expand-file-name "README.org" user-emacs-directory))
 (defconst config-el  (expand-file-name "config.el" user-emacs-directory))
 
-(when (and IS-WINDOWS (null (getenv "HOME")))
-  (setenv "HOME" (getenv "USERPROFILE")))
-
 (unless (file-exists-p config-el)
   (require 'org)
   (org-babel-tangle-file config-org config-el))
 
 (load-file config-el)
 
-(when (require 'time-date nil t)
-  (message "Emacs startup time: %.2f seconds."
-           (time-to-seconds (time-since emacs-load-start-time))))
+(add-hook 'after-init-hook
+          (lambda ()
+            (when (require 'time-date nil t)
+              (message "Emacs startup time: %.2f seconds."
+                       (time-to-seconds (time-since emacs-load-start-time))))))
