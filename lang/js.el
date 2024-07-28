bca96cc408299e4c88492e29dc2fc473c7ed5dc3
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 14:33:56 -0400

    typescript mode setup

--- lang/js.el
+++ lang/js.el
@@ -1,14 +1,13 @@
 ;;; -*- lexical-binding: t -*-
 (use-package js2-mode
-  :hook
-  (js2-mode . lsp-deferred)
-  :mode
-  ("\\.js\\'" . js2-mode)
-  ("\\.ts\\'" . js2-mode)
+  :hook (js2-mode . lsp-deferred)
+  :mode ("\\.js\\'" . js2-mode)
   :custom (js2-basic-offset 2))
 
 (use-package typescript-mode
-  :disabled t
+  :mode
+  ("\\.ts\\'" . typescript-mode)
+  :hook (typescript-mode . lsp-deferred)
   :custom
   (typescript-indent-level 2)
   (typescript-auto-indent-flag t))
