05b0a216aaad6e823e23cd9e94d1dad34157e69e
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 12:39:27 -0400

    js2 mode in ts files

--- lang/js.el
+++ lang/js.el
@@ -2,11 +2,13 @@
 (use-package js2-mode
   :hook
   (js2-mode . lsp-deferred)
-  :mode ("\\.js\\'" . js2-mode)
+  :mode
+  ("\\.js\\'" . js2-mode)
+  ("\\.ts\\'" . js2-mode)
   :custom (js2-basic-offset 2))
 
 (use-package typescript-mode
-  :defer t
+  :disabled t
   :custom
   (typescript-indent-level 2)
   (typescript-auto-indent-flag t))
