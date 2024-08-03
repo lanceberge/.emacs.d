a7eaf3d4599b6c9a4828125a65abaaa8bc5a853e
lanceberge <bergeron.lance6@gmail.com>
2024-08-03 18:50:22 -0400

    more meta key

--- editor/evil/evil.el
+++ editor/evil/evil.el
@@ -30,9 +30,11 @@
   ('evil-ex-completion-map ";" 'exit-minibuffer) ; use ; to complete : vim commands
 
   ('(normal visual motion)
-   ";" #'evil-ex ; switch ; and :
-   "H" #'evil-first-non-blank
-   "L" #'evil-end-of-line)
+   "M-u" #'evil-scroll-up
+   "M-d" #'evil-scroll-down
+   ";"   #'evil-ex ; switch ; and :
+   "H"   #'evil-first-non-blank
+   "L"   #'evil-end-of-line)
 
   (my-leader-def
     "bS" #'(evil-write-all                     :which-key "write all buffers")
