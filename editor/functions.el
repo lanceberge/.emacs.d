05cdb4599ecd57a82a76f8e930840bceb0e666ab
lanceberge <bergeron.lance6@gmail.com>
2024-08-08 18:49:06 -0400

    fix push mark functions

--- editor/functions.el
+++ editor/functions.el
@@ -164,7 +164,7 @@
 
 ;; https://stackoverflow.com/questions/3393834/how-to-move-forward-and-backward-in-emacs-mark-ring
 ;;;###autoload
-(defun +marker-is-point-p (marker)
+(defun marker-is-point-p (marker)
   "test if marker is current point"
   (and (eq (marker-buffer marker) (current-buffer))
        (= (marker-position marker) (point))))
@@ -173,7 +173,7 @@
 (defun +push-mark-maybe ()
   "push mark onto `global-mark-ring' if mark head or tail is not current location"
   (if (not global-mark-ring) (error "global-mark-ring empty")
-    (unless (or (marker-is-point-p (car global-mark-ring))
+    (unless (or (+marker-is-point-p (car global-mark-ring))
                 (marker-is-point-p (car (reverse global-mark-ring))))
       (push-mark))))
 
@@ -182,7 +182,7 @@
 (defun +backward-global-mark ()
   "use `pop-global-mark', pushing current point if not on ring."
   (interactive)
-  (push-mark-maybe)
+  (+push-mark-maybe)
   (when (marker-is-point-p (car global-mark-ring))
     (call-interactively 'pop-global-mark))
   (call-interactively 'pop-global-mark))
@@ -191,7 +191,7 @@
 (defun +forward-global-mark ()
   "hack `pop-global-mark' to go in reverse, pushing current point if not on ring."
   (interactive)
-  (push-mark-maybe)
+  (+push-mark-maybe)
   (setq global-mark-ring (nreverse global-mark-ring))
   (when (marker-is-point-p (car global-mark-ring))
     (call-interactively 'pop-global-mark))
