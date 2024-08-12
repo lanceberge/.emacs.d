6e46f0a62ca05432ecccc954396676c4576dd3e7
lanceberge <bergeron.lance6@gmail.com>
2024-08-12 16:37:37 -0400

    svelte mode, refactor

--- lang/js.el
+++ lang/js.el
@@ -1,13 +1,11 @@
 ;;; -*- lexical-binding: t -*-
 (use-package js2-mode
-  :hook (js2-mode . lsp-deferred)
   :mode ("\\.js\\'" . js2-mode)
   :custom (js2-basic-offset 2))
 
 (use-package typescript-mode
   :mode
   ("\\.ts\\'" . typescript-mode)
-  :hook (typescript-mode . lsp-deferred)
   :custom
   (typescript-indent-level 2)
   (typescript-auto-indent-flag t))
