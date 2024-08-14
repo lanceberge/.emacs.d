6c2feda7d35f2a58183cd9f9d79b328fc72a09ee
Lance Bergeron <lance.bergeron@ccri.com>
2024-08-14 16:49:18 -0400

    non built in org

--- lang/org/org.el
+++ lang/org/org.el
@@ -1,6 +1,5 @@
 ;;; -*- lexical-binding: t -*-
 (use-package org
-  :straight (:type built-in)
   :defer-incrementally
   (calendar find-func format-spec org-macs org-compat
             org-faces org-entities org-list org-pcomplete org-src
