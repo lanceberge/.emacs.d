e6361c42543c43d880962ecfaf274d3e68ed3a4b
lanceberge <bergeron.lance6@gmail.com>
2024-08-17 15:21:31 -0400

    misc

--- editor/keybindings.el
+++ editor/keybindings.el
@@ -6,31 +6,31 @@
     "c" (general-simulate-key "C-c" :which-key "+Mode specific maps"))
 
   (my-leader-def
-    "nf"      #'(make-frame                 :which-key "New Frame")
-    "h"       #'(help-command               :which-key "Help")
-    ";"       #'(shell-command              :which-key "shell command")
+    "nf"      #'(make-frame                :which-key "New Frame")
+    "h"       #'(help-command              :which-key "Help")
+    ";"       #'(shell-command             :which-key "shell command")
 
     ;; Windows
-    "w"       #'(evil-window-map            :which-key "+Windows") ; window command
+    "w"       #'(evil-window-map           :which-key "Windows") ; window command
 
     ;; Buffers
-    "bd"      #'(kill-current-buffer        :which-key "delete buffer")
-    "bq"      #'(+save-and-kill-buffer      :which-key "save and kill buffer")
-    "b SPC d" #'(+kill-window-and-buffer    :which-key "kill window and buffer")
+    "bd"      #'(kill-current-buffer       :which-key "delete buffer")
+    "bq"      #'(+save-and-kill-buffer     :which-key "save and kill buffer")
+    "b SPC d" #'(+kill-window-and-buffer   :which-key "kill window and buffer")
     "br"        (defun +revert-buffer () (interactive)
-                  (revert-buffer t t)       :which-key "revert buffer")
-    "bn"      #'(next-buffer                :which-key "next buffer")
-    "bp"      #'(previous-buffer            :which-key "previous buffer")
+                       (revert-buffer t t) :which-key "revert buffer")
+    "bn"      #'(next-buffer               :which-key "next buffer")
+    "bp"      #'(previous-buffer           :which-key "previous buffer")
 
     ;; Eval elisp
-    "es"      #'(eval-last-sexp             :which-key "execute elisp sexp")
-    "ee"      #'(eval-expression            :which-key "evaluate elisp expression")
-    "eb"      #'(eval-buffer                :which-key "evaluate elisp buffer")
-    "ef"      #'(eval-defun                 :which-key "evaluate elisp defun")
+    "es"      #'(eval-last-sexp            :which-key "execute elisp sexp")
+    "ee"      #'(eval-expression           :which-key "evaluate elisp expression")
+    "eb"      #'(eval-buffer               :which-key "evaluate elisp buffer")
+    "ef"      #'(eval-defun                :which-key "evaluate elisp defun")
 
     ;; Find specific files
     "er" (defun +source-init-file () (interactive)
-           (load-file "~/.emacs.d/init.el") :which-key "source init file"))
+                (load-file "~/.emacs.d/init.el") :which-key "source init file"))
 
   (my-leader-def
     :states 'visual
@@ -44,7 +44,9 @@
    "g C-l" #'(end-of-visual-line       :which-key "end of visual line")
    "g C-h" #'(beginning-of-visual-line :which-key "beginning of visual line"))
 
-  ('(normal insert)
+  ('normal
+   "Y" (general-simulate-key "y$" :which-key "yank until end of line")
+
    "C-/" #'(comment-line :which-key "comment")
    "M-/" #'(comment-line :which-key "comment"))
 
