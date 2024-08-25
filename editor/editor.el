85774d501e05233975ac951c7448a8010922e476
lanceberge <bergeron.lance6@gmail.com>
2024-08-24 21:11:03 -0400

    treesit

--- editor/editor.el
+++ editor/editor.el
@@ -18,6 +18,8 @@
   (setq-default format-all-formatters
                 '(("TypeScript" prettier)
                   ("svelte"     prettier)
+                  ("Svelte"     prettier)
+                  ("Go"         gofmt)
                   ("HTML"       prettier)
                   ("JSON"       prettier)
                   ("JavaScript" prettier))))
@@ -258,6 +260,29 @@
          ;; More modes defined here...
          )
   :config
+  (dolist (grammar
+           '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+             (bash "https://github.com/tree-sitter/tree-sitter-bash")
+             (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+             (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+             (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+             (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+             (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+             (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+             (make "https://github.com/alemuller/tree-sitter-make")
+             (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+             (cmake "https://github.com/uyha/tree-sitter-cmake")
+             (c "https://github.com/tree-sitter/tree-sitter-c")
+             (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+             (toml "https://github.com/tree-sitter/tree-sitter-toml")
+             (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+             (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+             (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+             (prisma "https://github.com/victorhqc/tree-sitter-prisma")))
+    (add-to-list 'treesit-language-source-alist grammar)
+    (unless (treesit-language-available-p (car grammar))
+      (treesit-install-language-grammar (car grammar))))
+
   (dolist (mapping
            '((python-mode . python-ts-mode)
              (css-mode . css-ts-mode)
