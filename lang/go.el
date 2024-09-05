6e98106e512253956c16eb89790ae77643d08ccb
lanceberge <bergeron.lance6@gmail.com>
2024-09-05 17:47:06 -0400

    cleanup

--- lang/go.el
+++ lang/go.el
@@ -2,18 +2,4 @@
 (use-package go-mode
   :defer t
   :config
-  ;; https://github.com/meain/tree-surgeon/blob/master/tree-surgeon-go-error.el
-;;;###autoload
-  (defun tree-surgeon-go-error (&optional errformat)
-    "Insert the return statement.
-Pass in an `ERRFORMAT' of you want to override the default."
-    (interactive)
-    (let ((return-string (tree-surgeon-go-error--get-return-string
-			  (or errformat tree-surgeon-go-error-format-default))))
-      (if return-string
-	  (let ((start (progn (end-of-line) (newline) (point))))
-	    (insert "if err != nil {\n")
-	    (insert return-string)
-	    (insert "\n}")
-	    (indent-region start (point)))
-	(message "Could not compute return statement")))))
+  (setq-local use-tabs-mode t))
