09ab3e5263c9c922347a4f92855a7930b532726a
lanceberge <bergeron.lance6@gmail.com>
2024-09-07 18:06:18 -0400

    WIP C++ DAP

--- lang/c.el
+++ lang/c.el
@@ -2,6 +2,9 @@
 (use-package cc-mode
   :ensure nil
   :mode ("\\.cu\\'" . c-mode)
+  :init
+  (dap-cpptools-setup)
+  (require 'dap-cpptools)
   :hook
   (c++-mode . +cpp-mode)
   (c++-ts-mode . +cpp-mode)
@@ -10,7 +13,17 @@
   (c-default-style "linux")
   :config
   (defun +cpp-mode ()
-    (setq-local tab-width 2)))
+    (setq-local tab-width 4))
+
+  ;; TODO
+  (dap-register-debug-template
+   "cpptools::Run Configuration"
+   (list :type "cppdbg"
+         :request "launch"
+         :name "cpptools::Run Configuration"
+         :MIMode "gdb"
+         :program "${workspaceFolder}/average_contiguous_subarray"
+         :cwd "${workspaceFolder}")))
 
 (use-package gdb-mi
   :ensure nil
