8758e6db83c17f00a9a87a1df17fc5e554f132ea
lanceberge <bergeron.lance6@gmail.com>
2024-09-14 11:34:59 -0400

    c compilation fixes

--- lang/c.el
+++ lang/c.el
@@ -13,31 +13,34 @@
     (add-to-list 'exec-path "/opt/homebrew/opt/llvm/bin"))
 
   (defun +cpp-mode ()
-    (let* ((file-name (file-name-nondirectory (buffer-file-name)))
-           (file-name-base (file-name-base file-name)))
-
+    (let ((file-name-base (file-name-base (file-name-nondirectory (buffer-file-name)))))
       (setq-local dape-configs
                   `((lldb-dap
                      command "lldb-dap" :type "lldb-dap" modes
                      (c-mode c-ts-mode c++-mode c++-ts-mode)
                      ensure dape-ensure-command
-                     command-cwd file-name
-                     :cwd "." :program file-name-base)))))
+                     command-cwd ,(file-name-directory (buffer-file-name))
+                     :cwd "." :program ,file-name-base)))))
 
   :general
   (my-leader-def
     :keymaps '(c++-mode-map c++-ts-mode-map)
     "ec" #'+cpp-compile)
+
+  ;; TODO fix
   (my-localleader-def
-    :keymaps '(c++-mode-map c++-ts-mode-map)
-    "dd" (defun +dape-debug ()
-           (interactive)
-           (+cpp-compile)
-           (dape))))
+    :keymaps '(c++-mode-map c++-ts-mode-map c-ts-mode-map)
+    "dd" #'+dape-debug))
 
 ;;;###autoload
-(defun +cpp-compile ()
+(defun +dape-debug ()
+  (interactive)
+  (+cpp-compile "-g")
+  (call-interactively #'dape))
+;;;###autoload
+
+(defun +cpp-compile (&optional flags)
   (interactive)
   (let* ((file-name (file-name-nondirectory (buffer-file-name)))
          (file-name-base (file-name-base file-name)))
-    (compile (concat "g++-14 " file-name " -o " file-name-base " -std=c++20"))))
+    (compile (concat "g++-14 " file-name " -o " file-name-base " -std=c++20 " flags))))
