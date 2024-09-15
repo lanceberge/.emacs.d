d4327b1f8f720127470a6152adc0c7e19ff5e8e4
lanceberge <bergeron.lance6@gmail.com>
2024-09-15 11:00:34 -0400

    vterm in current dir function

--- editor/terminal.el
+++ editor/terminal.el
@@ -29,17 +29,23 @@
     "C-c" #'vterm--self-insert)
 
   (my-leader-def
-    "ov" #'(vterm :which-key "vterm"))
+    "ov" #'(+vterm :which-key "vterm"))
   :config
-  (cl-loop for num from 0 to 9 do
-           (general-define-key :keymaps 'vterm-mode-map
-                               :states '(normal insert)
-                               (number-to-string num) #'vterm--self-insert))
   (evil-collection-init 'vterm)
   (add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1))))
 
+
 ;;;###autoload
-(defun +tramp/ssh-cascades ()
-  "ssh into cascades through tramp"
+(defun +vterm ()
+  "Open or switch to a vterm buffer, changing directory if it's not equal to the cwd."
   (interactive)
-  (find-file "/sshx:lancebergeron@cascades1.arc.vt.edu:~/"))
+  (let ((vterm-buffer (get-buffer "*vterm*"))
+        (current-dir (expand-file-name default-directory)))
+    (if vterm-buffer
+        (progn
+          (switch-to-buffer vterm-buffer)
+          (unless (string= current-dir (expand-file-name default-directory))
+            (vterm-send-string (concat "cd " (shell-quote-argument current-dir)))
+            (vterm-send-return)))
+      (vterm)))
+  (evil-collection-vterm-append))
