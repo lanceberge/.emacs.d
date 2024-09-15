7092a747849527d6e9f3745306ef959668d2aebd
lanceberge <bergeron.lance6@gmail.com>
2024-09-15 11:45:29 -0400

    file for treesit

---
+++ editor/treesit.el
@@ -0,0 +1,59 @@
+;;; -*- lexical-binding: t -*-
+(use-package treesit
+  :ensure nil
+  :after yasnippet
+  :config
+  (dolist (grammar
+           '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+             (bash "https://github.com/tree-sitter/tree-sitter-bash")
+             (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+             (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+             (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+             (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+             (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+             (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+             (make "https://github.com/alemuller/tree-sitter-make")
+             (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+             (cmake "https://github.com/uyha/tree-sitter-cmake")
+             (c "https://github.com/tree-sitter/tree-sitter-c")
+             (cpp "https://github.com/tree-sitter/tree-sitter-cpp" "v0.22.0")
+             (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+             (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+    (add-to-list 'treesit-language-source-alist grammar)
+    (unless (treesit-language-available-p (car grammar))
+      (treesit-install-language-grammar (car grammar))))
+
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (css-mode . css-ts-mode)
+             (typescript-mode . typescript-ts-mode)
+             (js-mode . typescript-ts-mode)
+             (js2-mode . typescript-ts-mode)
+             (c-mode . c-ts-mode)
+             (c++-mode . c++-ts-mode)
+             (c-or-c++-mode . c-or-c++-ts-mode)
+             (bash-mode . bash-ts-mode)
+             (css-mode . css-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode . json-ts-mode)
+             (sh-mode . bash-ts-mode)
+             (sh-base-mode . bash-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping)
+
+    (let* ((source-mode (car mapping))
+           (target-mode (cdr mapping))
+           (source-mode-hook (intern (concat (symbol-name source-mode) "-hook")))
+           (target-mode-hook (intern (concat (symbol-name target-mode) "-hook"))))
+
+      (when (and (boundp source-mode-hook)
+                 (boundp target-mode-hook)
+                 (symbol-value source-mode-hook))
+        (dolist (hook (symbol-value source-mode-hook))
+          (add-hook target-mode-hook hook))
+
+        (add-hook source-mode-hook
+                  (lambda ()
+                    (yas-activate-extra-mode target-mode)))
+        (add-hook target-mode-hook
+                  (lambda ()
+                    (yas-activate-extra-mode source-mode)))))))
