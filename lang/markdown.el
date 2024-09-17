586b0480f7eab476f4da6db6222f8c0f43366bf3
lanceberge <bergeron.lance6@gmail.com>
2024-09-17 19:57:21 -0400

    markdown fontify natively

--- lang/markdown.el
+++ lang/markdown.el
@@ -1,9 +1,11 @@
 ;;; -*- lexical-binding: t -*-
 (use-package markdown-mode
   :ensure nil
+  :custom
+  (markdown-fontify-code-blocks-natively t)
   :general
   ('normal 'markdown-mode-map
-	   "RET" #'markdown-follow-thing-at-point))
+           "RET" #'markdown-follow-thing-at-point))
 
 (use-package markdown-toc ; create a table of contents
   :general
