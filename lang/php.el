693badc9702e68dc58cda1eaa1b61928c8731745
lanceberge <bergeron.lance6@gmail.com>
2024-11-01 21:57:34 -0400

    +php-mode, format buffer, and don't write .cache files

--- lang/php.el
+++ lang/php.el
@@ -1,21 +1,28 @@
 (use-package php-mode
   :mode ("\\.php\\'" . php-mode)
-  ;; :hook
-  ;; (php-mode . (lambda ()
-  ;;               (add-hook 'after-save-hook #'php-cs-fixer-fix-current nil t)))
-  )
+  :hook
+  (php-mode . +php-mode)
+  :general
+  (my-leader-def
+    "=" (lambda () (interactive)
+          (save-buffer)
+          (php-cs-fixer-fix-current))))
 
-;; ;;;###autoload
-;; (defun php-cs-fixer-fix-current ()
-;;   "Run PHP CS Fixer on the current file silently."
-;;   (interactive)
-;;   (when (and buffer-file-name
-;;              (string-match "\\.php\\'" buffer-file-name))
-;;     (let ((command (concat "~/.config/composer/vendor/bin/php-cs-fixer fix "
-;;                            (shell-quote-argument buffer-file-name)))
-;;           (current-point (point)))
-;;       (call-process-shell-command command nil 0)
-;;       (run-with-timer 0.1 nil
-;;                       (lambda ()
-;;                         (revert-buffer nil t t)
-;;                         (goto-char current-point))))))
+(defun +php-mode ()
+  (add-hook 'after-save-hook #'php-cs-fixer-fix-current nil t)
+  (format-all-mode -1))
+
+;;;###autoload
+(defun php-cs-fixer-fix-current ()
+  "Run PHP CS Fixer on the current file silently."
+  (interactive)
+  (when (and buffer-file-name
+             (string-match "\\.php\\'" buffer-file-name))
+    (let ((command (concat "~/.config/composer/vendor/bin/php-cs-fixer fix --using-cache=no "
+                           (shell-quote-argument buffer-file-name)))
+          (current-point (point)))
+      (call-process-shell-command command nil 0)
+      (run-with-timer 0.1 nil
+                      (lambda ()
+                        (revert-buffer nil t t)
+                        (goto-char current-point))))))
