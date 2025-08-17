631c1ba323256bf1832c37befa1f034c3f1ce45c
lanceberge <bergeron.lance6@gmail.com>
2025-08-16 22:21:54 -0400

    project replace regex function

--- editor/project.el
+++ editor/project.el
@@ -10,7 +10,8 @@
     "pf" #'(project-find-file :which-key "find file")
     "ps" #'(consult-ripgrep :which-key "ripgrep")
     "pe" #'(flymake-show-project-diagnostics :which-key "show errors")
-    "pr" #'project-query-replace-regexp
+    "pr" #'+project-replace-regex
+    "SPC r" #'project-query-replace-regexp
     "pt" (defun +project-find-todos ()
            (interactive)
            (consult-ripgrep (project-root (project-current t)) "TODO"))
@@ -22,8 +23,8 @@
 ;;;###autoload
 (defun +project-switch-and-vterm ()
   (interactive)
-  (setq project-switch-commands #'+vterm-project)
-  (call-interactively 'project-switch-project))
+  (let ((project-switch-commands #'+vterm-project))
+    (call-interactively 'project-switch-project)))
 
 ;;;###autoload
 (defun +project-load-projects ()
@@ -34,3 +35,18 @@
   (dolist (dir '("~/code/git" "~/code/phoenix" "~/code/laravel" "~/code/"))
     (when (file-directory-p dir)
       (project-remember-projects-under dir))))
+
+;;;###autoload
+(defun +project-replace-regex (search-regex replace-string)
+  (interactive (list (read-string "Replace: ")
+                     (read-string "Replace With: ")))
+  (let* ((project-root (vc-root-dir))
+         (default-directory project-root))
+    (if project-root
+        (progn
+          (shell-command
+           (format "git ls-files -z | xargs -0 perl -pi -e 's/%s/%s/g'"
+                   (shell-quote-argument search-regex)
+                   (shell-quote-argument replace-string)))
+          (message (format "Replaced")))
+      (message "fatal: not in a git repository"))))
