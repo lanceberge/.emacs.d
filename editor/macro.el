eb8c7fd8643f9590d14d4a423ca813ef459eec0e
lanceberge <bergeron.lance6@gmail.com>
2025-08-23 17:28:09 -0400

    macro functions support applying to regions

--- editor/macro.el
+++ editor/macro.el
@@ -8,31 +8,46 @@
 
 (defvar +kmacro--last-name nil)
 
-(defun +kmacro-record-or-end ()
+(defun +kmacro-record-or-end (&optional n)
   "If a macro is being recorded, end it. If c is input, call the last macro.
 Otherwise, create a named macro with the char input."
-  (interactive)
+  (interactive "p")
   (if defining-kbd-macro
       (progn
         (call-interactively #'kmacro-end-macro)
         (kmacro-name-last-macro +kmacro--last-name))
     (let* ((name (read-char "Name: "))
            (macro-name (+kmacro--get-name name)))
-      (if (= name ?c)
+      (if (= name ?l)
           (with-undo-amalgamate
-            (call-interactively #'kmacro-call-macro))
+            (if (region-active-p)
+                (call-interactively #'apply-macro-to-region-lines)
+              (dotimes (_ n)
+                (call-interactively #'kmacro-call-macro))))
         (progn
           (call-interactively #'kmacro-start-macro)
           (setq +kmacro--last-name macro-name))))))
 
-(defun +kmacro-call-named-macro (char-name n)
+(defun +kmacro-call-named-macro (char-name &optional n)
   (interactive "cName: \np")
-  (let ((macro-name (+kmacro--get-name char-name)))
+  (let ((macro-symbol (+kmacro--get-name char-name)))
     (with-undo-amalgamate
-      (if (= char-name ?c)
-          (call-interactively #'kmacro-call-macro)
-        (dotimes (_ n)
-          (funcall macro-name))))))
+      (if (= char-name ?l)
+          (if (region-active-p)
+              (call-interactively #'apply-macro-to-region-lines)
+            (call-interactively #'kmacro-call-macro))
+        (if (region-active-p)
+            (+kmacro--apply-to-region macro-symbol)
+          (dotimes (_ n)
+            (funcall macro-symbol)))))))
+
+(defun +kmacro--apply-to-region (macro-symbol)
+  (if (not (region-active-p))
+      (error "No active region"))
+  (if (not (fboundp macro-symbol))
+      (error "Symbol %s is not a defined function or macro" macro-symbol))
+  (kmacro-push-ring (kmacro-extract-lambda (symbol-function macro-symbol)))
+  (call-interactively #'apply-macro-to-region-lines))
 
 (defun +kmacro--get-name (char)
   (intern (format "+kmacro--macro-%s" (char-to-string char))))
